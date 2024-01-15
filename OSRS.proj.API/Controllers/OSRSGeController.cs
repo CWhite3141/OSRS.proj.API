2ad300180d0129ec948513871c3a8c2b611058a7
cwhite3141 <cwhite3141@gmail.com>
2024-01-14 18:37:21 -0600

    Set Status Code

--- OSRS.proj.API/Controllers/OSRSGeController.cs
+++ OSRS.proj.API/Controllers/OSRSGeController.cs
@@ -20,24 +20,24 @@ namespace OSRS.proj.API.Controllers
 
         [HttpPost("GetCategoryInfo")]
         [ProducesResponseType(typeof(CategoryResponse), 200)]
-        public async Task<CategoryResponse> GetCategoryInfo(CategoryRequest request)
+        public async Task<ActionResult<CategoryResponse>> GetCategoryInfo(CategoryRequest request)
         {
             var response = await _service.GetCategoryInfo(request);
-            return response;
+            return Ok(response);
         }
         [HttpPost("GetItems")]
         [ProducesResponseType(typeof(ItemsResponse), 200)]
-        public async Task<List<ItemsResponse>> GetItems(ItemsRequest request)
+        public async Task<ActionResult<List<ItemsResponse>>> GetItems(ItemsRequest request)
         {
             var response = await _service.GetItems(request);
-            return response;
+            return Ok(response);
         }
         [HttpPost("GetItemDetails")]
         [ProducesResponseType(typeof(ItemDetailsResponse), 200)]
-        public async Task<ItemDetailsResponse> GetItemDetails(ItemDetailsRequest request)
+        public async Task<ActionResult<ItemDetailsResponse>> GetItemDetails(ItemDetailsRequest request)
         {
             var response = await _service.GetItemDetails(request);
-            return response;
+            return Ok(response);
         }
     }
 }
