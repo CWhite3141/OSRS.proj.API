b467ae3dfa0c3478b9954b1b5c86398423070601
allegedSTINK <dylbeebe@gmail.com>
2024-01-24 09:15:39 -0600

    adds loading animation

--- frontend/src/components/Loading/Loading.tsx
+++ frontend/src/components/Loading/Loading.tsx
@@ -1,10 +1,23 @@
-'use client'
-import React from 'react'
+"use client";
+import React from "react";
 
 const Loading = () => {
-  return (
-    <div>LOADING BITCH</div>
-  )
-}
+	return (
+		<div className="relative mx-auto mt-20" style={{width: "200px", height: "200px"}}>
+			<svg
+				className="circular-loader"
+				viewBox="25 25 50 50">
+				<circle
+					className="loader-path stroke-Maroon"
+					cx="50"
+					cy="50"
+					r="20"
+					fill="none"
+					stroke-width="2"
+				/>
+			</svg>
+		</div>
+	);
+};
 
-export default Loading
+export default Loading;
